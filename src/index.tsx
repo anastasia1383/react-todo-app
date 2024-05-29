3311dba506be1bc28a189ed71c2bc0b2127b8105
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-05-29 15:15:18 +0200

    react todo app

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,15 @@ import { createRoot } from 'react-dom/client';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import './index.scss';
+import './styles/index.scss';
 
 import { App } from './App';
+import { TodosContextProvider } from './context/TodoContext';
 
-createRoot(document.getElementById('root') as HTMLDivElement).render(<App />);
+const Root = () => (
+  <TodosContextProvider>
+    <App />
+  </TodosContextProvider>
+);
+
+createRoot(document.getElementById('root') as HTMLDivElement).render(<Root />);
